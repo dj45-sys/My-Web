287bbe8f372974182b59c2fb318732e31f970004
Dj45-SyS <dj45-sys@outlook.com>
2022-02-26 18:31:08 -0500

    some configs fixed :)

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,6 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     '@nuxtjs/axios',
-    'nuxt-material-design-icons'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
